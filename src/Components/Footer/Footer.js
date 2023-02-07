b05c747cb2cb9b615e5ec1b2b9747d915bf227fb
ShlomoSerber <80367349+ShlomoSerber@users.noreply.github.com>
2023-02-07 11:44:40 -0300

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css"
 function Footer() {
     return (
         <div className="footer">
-            <p className="footerText">Hecho por programa desde tu casa</p>
+            <p className="footerText">Hecho por Shlomo Serber</p>
         </div>
     );
 }
